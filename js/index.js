1edc9eaaa4df65e4293b7e36f3d67ad878432c38
Alex <david_alejandrorizo@outlook.com>
2022-10-25 14:59:41 -0500

    line animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,110 @@
 // Referencias HTML;
+const us = document.querySelector('.us')
+const services = document.querySelector('.services')
+const experience = document.querySelector('.experience')
+const contact = document.querySelector('.contact')
+
+const underline = document.querySelector('.underline')
+
+underline.style.transition = '1s'
+
+let init = 0;
+
+us.addEventListener('click', () => {
+    click(0)
+})
+
+services.addEventListener('click', () => {
+    click(1)
+})
+
+experience.addEventListener('click', () => {
+    click(2)
+})
+
+contact.addEventListener('click', () => {
+    click(3)
+})
+
+const click = (pos) => {
+    if (init == 0 ) {
+        switch (pos) {
+            case 1:
+                console.log('servicios');
+                underline.style.marginLeft = '264px'
+                break;
+            case 2:
+                console.log('experiencia');
+                underline.style.marginLeft = '528px'
+                break;
+            case 3:
+                console.log('contacto');
+                underline.style.marginLeft = '832px'
+                break;
+            default: break;
+        }
+    }
+
+    if (init == 1 ) {
+        switch (pos) {
+            case 0:
+                console.log('nosotros');
+                underline.style.marginLeft = '0px'
+                break;
+            case 2:
+                console.log('experiencia');
+                underline.style.marginLeft = '528px'
+                break;
+            case 3:
+                console.log('contacto');
+                underline.style.marginLeft = '832px'
+                break;
+            default: break;
+        }
+    }
+
+    if (init == 2 ) {
+        switch (pos) {
+            case 0:
+                console.log('nosotros');
+                underline.style.marginLeft = '0px'
+                break;
+            case 1:
+                console.log('servicios');
+                underline.style.marginLeft = '264px'
+                break;
+            case 3:
+                console.log('contacto');
+                underline.style.marginLeft = '832px'
+                break;
+            default: break;
+        }
+    }
+
+    if (init == 3 ) {
+        switch (pos) {
+            case 0:
+                console.log('nosotros');
+                underline.style.marginLeft = '0px'
+                break;
+            case 1:
+                console.log('nosotros');
+                underline.style.marginLeft = '264px'
+                break;
+            case 2:
+                console.log('experiencia');
+                underline.style.marginLeft = '528px'
+                break;
+            default: break;
+        }
+    }
+
+    init = pos;
+}
+
+const plusIcon = document.querySelectorAll('.plus-icon');
+const minusIcon = document.querySelectorAll('.fa-minus');
+
 const papCorpTitle = document.querySelector('.p-pap-corp');
 const papPubTitle = document.querySelector('.p-pap-pub');
 const papAdmTitle = document.querySelector('.p-pap-adm');
@@ -54,6 +160,7 @@ const papTitles = {
 window.onscroll = () => {
     const y = window.scrollY;
     closeMenu();
+    console.log(y);
 
     if (y < 100) {
         head.classList.remove('menu-background-transition');
@@ -61,6 +168,24 @@ window.onscroll = () => {
     
     if (y >= 100) {
         head.classList.add('menu-background-transition');
+    }
+
+    switch (y) {
+        case 1330 :
+            underline.style.marginLeft = '0px'
+            break;
+    
+        case 2437 :
+            underline.style.marginLeft = '264px'
+            break;
+        case 3534 :
+            underline.style.marginLeft = '528px'
+            break;
+        case 3955 :
+            underline.style.marginLeft = '832px'
+            break;
+        default:
+            break;
     }
 
 };
@@ -77,10 +202,16 @@ const removeClasses = (elements, className) => {
     });
 }
 
+const toggleClass = (element, className) => {
+    element.classList.toggle(className);
+}
+
 const openPapeleria = (papeleria) => {
     switch (papeleria) {
         case 'corporativa':
             papIcons.papCor.toggleAttribute('hidden');
+            toggleClass(plusIcon[0], 'toggle-display-false');
+            toggleClass(minusIcon[0], 'toggle-display-true');
             papDescriptions.papCorDesc.toggleAttribute('hidden');
             papTitles.papCorpTitle.classList.toggle('toggle-txt-color-black');
             papContainers.papCorpContainer.classList.toggle('toggle-bg-gray');
@@ -88,6 +219,8 @@ const openPapeleria = (papeleria) => {
 
         case 'publicitaria':
             papIcons.papPub.toggleAttribute('hidden')
+            toggleClass(plusIcon[1], 'toggle-display-false');
+            toggleClass(minusIcon[1], 'toggle-display-true');
             papDescriptions.papPubDesc.toggleAttribute('hidden');
             papTitles.papPubTitle.classList.toggle('toggle-txt-color-black');
             papContainers.papPubContainer.classList.toggle('toggle-bg-gray');
@@ -95,6 +228,8 @@ const openPapeleria = (papeleria) => {
 
         case 'administrativa':
             papIcons.papAdm.toggleAttribute('hidden')
+            toggleClass(plusIcon[2], 'toggle-display-false');
+            toggleClass(minusIcon[2], 'toggle-display-true');
             papDescriptions.papAdmDesc.toggleAttribute('hidden');
             papTitles.papAdmTitle.classList.toggle('toggle-txt-color-black');
             papContainers.papAdmContainer.classList.toggle('toggle-bg-gray');
@@ -102,6 +237,8 @@ const openPapeleria = (papeleria) => {
     
         case 'social':
             papIcons.papSoc.toggleAttribute('hidden')
+            toggleClass(plusIcon[3], 'toggle-display-false');
+            toggleClass(minusIcon[3], 'toggle-display-true');
             papDescriptions.papSocDesc.toggleAttribute('hidden');
             papTitles.papSocTitle.classList.toggle('toggle-txt-color-black');
             papContainers.papSocContainer.classList.toggle('toggle-bg-gray');
